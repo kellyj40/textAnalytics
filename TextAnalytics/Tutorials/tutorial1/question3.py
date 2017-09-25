cf7aa195818395b70ff116fafa3d8b32d888269b
Joe <kellyj40@tcd.ie>
2017-09-25 14:49:28 +0100

    Adding to python pickle

---
+++ TextAnalytics/Tutorials/tutorial1/question3.py
@@ -0,0 +1,58 @@
+# Imports
+from bs4 import BeautifulSoup as bs
+import requests
+import nltk
+import pickle
+
+from nltk.tokenize import RegexpTokenizer
+
+# URL link to bloomberg
+url = "https://www.bloomberg.com/europe"
+
+# Make the request
+html_object = requests.get(url)
+data_html = html_object.text
+
+# Make Beautiful soup object and pass in html code
+soup = bs(data_html, "html5lib")
+
+# Special characters remover and end of words
+tokenizer = RegexpTokenizer(r'\w+')
+
+# Remove tokenisation and special characters
+array_of_words = []
+for element in soup.select('h1'):
+    line = element.get_text().replace(".", "")
+    array_of_words.append(tokenizer.tokenize(line))
+
+# Reomove stop words, I, me, my etc..
+stopset = set(nltk.corpus.stopwords.words('english'))
+
+# Add two words to the stop words
+stopset.add("menu")
+stopset.add("trending")
+
+# Remove the stop words, compare to lower case or doesnt work, make it a list
+removed_stops = [word for word_array in array_of_words for word in word_array if word.lower() not in stopset]
+
+
+with open('bloomberg_news.pkl', 'rb') as f:
+    old_dict_of_words = pickle.load(f)
+
+print(len(old_dict_of_words))
+
+# dictionary to keep counter of the words
+dict_of_words = {}
+for word in removed_stops:
+    if word not in dict_of_words:
+        dict_of_words[word] = 1
+    else:
+        dict_of_words[word] += 1
+
+for key in dict_of_words:
+    if key in old_dict_of_words:
+        old_dict_of_words[key] += dict_of_words[key]
+    else:
+        old_dict_of_words[key] = dict_of_words[key]
+
+print(len(old_dict_of_words))
