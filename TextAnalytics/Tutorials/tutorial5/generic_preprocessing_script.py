d08594784dff56a55b2496c45a67ab7bd815b66e
Joe <kellyj40@tcd.ie>
2017-10-17 18:04:58 +0100

    generic script frequency counter added

--- TextAnalytics/Tutorials/tutorial5/generic_preprocessing_script.py
+++ TextAnalytics/Tutorials/tutorial5/generic_preprocessing_script.py
@@ -7,10 +7,14 @@ def pre_process_array(array_of_sentences):
 
     # 1st remove the stop words
     stops_removed_words = remove_stop_words_and_to_lower(array_of_sentences)
+
+    # 2nd Stem words
     stemmed_words = porter_stemming_words(stops_removed_words)
 
-    return stemmed_words
-    # 2nd stem words
+    # 3rd frequency of words
+    freq_of_words = calculate_frequency_words(stemmed_words)
+
+    return stemmed_words, freq_of_words
 
 # stop words to be removed
 stop_words = stopwords.words('english')
@@ -28,11 +32,29 @@ def remove_stop_words_and_to_lower(array_of_sentences):
     return removed_tokens
 
 
-def porter_stemming_words(array_of_words):
+def porter_stemming_words(array_of_documents):
     ps = PorterStemmer()
-    documents = [ps(word) for word in array_of_words]
-    return documents
+    stemmed_words = []
+    for document in array_of_documents:
+        stemmed_words.append([ps(word) for word in document])
+    return stemmed_words
 
+
+def calculate_frequency_words(documents):
+    # Make dict to contain words
+    dict_of_all_words = {}
+    # Seperate out the documents
+    for document in documents:
+        # Add each word to the dictionary in the document
+        for word in document:
+            # if its not already int the dictionary then add it
+            if word not in dict_of_all_words:
+                dict_of_all_words[word] = 1
+            else:
+            # Other wise add one to the current value
+                dict_of_all_words[word] += 1
+
+    return dict_of_all_words
 
 
 
