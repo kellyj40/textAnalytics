5a34f91eca39a96010c7c228d39e1b730937a814
Joe <kellyj40@tcd.ie>
2017-10-19 10:29:59 +0100

    Euclidean distance in comparison to Cosine

--- TextAnalytics/Tutorials/tutorial5/Euclidean_distance.py
+++ TextAnalytics/Tutorials/tutorial5/Euclidean_distance.py
@@ -1,6 +1,7 @@
+import math
 from generic_preprocessing_script import pre_process_array
 
-
+# Documents / tweets
 documents = ["Machine random words are in here learning will help computers learn algorithms and organized into taxonomy, based on the desired outcome of the algorithm. Common algorithm types include Supervised learning where the algorithm its self learns to generates a function that maps inputs to desired outputs. One standard formulation of the supervised learning task is the classification problem will learn: the learner is required to learn (to approximate the behavior of) a function which maps a vector into one of several classes by looking at several input-output examples of the function.",
              "Supervised some others are also now inserted in this one learning is the machine learning task of inferring a function from labeled training data. The training data consist of a set of training examples. In supervised learning, each example is a pair consisting of an input object (typically a vector) and learn a desired output value (also called the supervisory signal). A supervised learning algorithm analyzes the training data and produces an inferred function, which can be used for mapping new examples",
              "Unsupervised machine learning is the machine learning task of inferring a function to describe hidden structure from unlabeled data (a classification or categorization is not included in the observations). Since the examples given to the learner are unlabeled, there is no evaluation of the accuracy of the structure that is output by the relevant algorithm—which is one way of distinguishing unsupervised learning from supervised learning and reinforcement learning.",
@@ -8,10 +9,10 @@ documents = ["Machine random words are in here learning will help computers lear
              "Machine learning algorithms are organized into two categories, based on the desired outcome of the algorithm. Common algorithm types include Supervised learning and unsupervised learning techniques, where the algorithm generates a function that will map inputs to desired outputs or related results. One standard formulation of the supervised learning task is the classification problem of seperation. Unsupervised learning which models a set of inputs and outputs: labeled examples are not available.",
              "ML algorithms are organized into two seperate groups, based on the desired outcome of the algorithm. Common algorithm types include Supervised learning and unsupervised learning techniques, where the algorithm generates a function that maps inputs to desired outputs. One standard formulation of the supervised learning task is the classification problem. Unsupervised learning which models a set of inputs: labeled examples are not available.",
              "Machine learning algorithms are organized into two categories. These are Supervised learning and unsupervised learning techniques, where the algorithm generates a function for mapping. One standard formulation of the supervised learning task is the classification problem. Unsupervised learning which models a set of inputs: labeled examples are not available."]
-
+# Use generic pre-processing script
 cleaded_words, freq_of_words_per_doc, freq_of_words_all = pre_process_array(documents)
 
-import math
+
 # computes TF-IDF
 def compute_vector_len(doc_dict, df_count):
     tf_idf_dictionary = {}
@@ -29,8 +30,8 @@ for doc_freq in freq_of_words_per_doc:
     idf_scores.append(idf_per_doc)
 
 print(idf_scores)
-
-def get_cosine():
+# Calculate the Ecliliadian distance
+def get_Eucilian():
     counter = 0
     cosine_scores = []
     while counter < len(documents)-1:
@@ -39,39 +40,28 @@ def get_cosine():
             vec1 = idf_scores[counter]
             vec2 = idf_scores[counter2]
 
-            intersection = set(vec1.keys() & vec2.keys())
+            all_words = set(vec1.keys() | vec2.keys())
+
             # print(intersection)
-            numerator = sum([vec1[x] * vec2[x] for x in intersection])
+            inner_sum = 0
+            for word in all_words:
+                if word in vec1 and word in vec2:
+                    if (vec1[word] - vec2[word]) != 0:
+                        inner_sum += math.pow(vec1[word] - vec2[word], 2)
+                elif word in vec2:
+                    inner_sum += math.pow(vec2[word], 2)
+                elif word in vec1:
+                    inner_sum += math.pow(vec1[word], 2)
+            srt_sum = math.sqrt(inner_sum)
 
-            sum1 = sum([vec1[x]**2 for x in vec1.keys()])
-
-            sum2 = sum([vec2[x]** 2 for x in vec2.keys()])
-
-            denominator = math.sqrt(sum1) * math.sqrt(sum2)
-            if not denominator:
-                cosine_scores.append([counter, counter2, 0.0])
-            else:
-                cosine_scores.append([counter, counter2, round(float(numerator)/ denominator, 3)])
+            cosine_scores.append([counter, counter2, round(srt_sum)])
             counter2+=1
         counter+=1
     return cosine_scores
 
-cos_values = get_cosine()
+cos_values = get_Eucilian()
 import pprint as pp
 pp.pprint(cos_values)
 
-# Graph the results
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-x, y, z = zip(*cos_values)
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(x,y,z)
-ax.set_xlabel('document')
-ax.set_ylabel('document')
-ax.set_zlabel('cos(theta)')
-plt.show()
 
 
--- TextAnalytics/Tutorials/tutorial5/Levenstein.py
+++ TextAnalytics/Tutorials/tutorial5/Levenstein.py
@@ -1,13 +1,15 @@
 # Used to calculate the distance between one another
 import editdistance
 
-
+import matplotlib.pyplot as plt
+# Normal tweets
 normals = ["liverpool to win the champions league after that result, great win for the reds, #LFC #Coutinho #Salah #7-0 #Liverpool #Maribor",
             "ireland to face denmark in world cup qualifier, best possible draw for ireland, #COYBIG #November9th #EriksonvMcClean  #wc2018 #russia",
             "hurricane ophelia to hit ireland early next week, schools are set to close as the country prepares for distruption. Colleges to follow suit if nothing changes.",
             "anyone got any tickets to tomorrows match? Thinking of making the trip to slovenia #LFC #Maribor #CL #CHenderson #slovenia #CLQ",
             "world cup draw results #IREvDEN #wc2018"]
 
+# Spam tweets
 spams = ["@worldCup @IRE world cup draw results here @WC2018 #wc2018 #russia http://www.fifa.com/worldcup/preliminaries/matches/index.html",
         "@worldCup @IRE all world cup results here #IREvDEN @WC2018 #wc2018 #russia http://www.fifa.com/worldcup/preliminaries/matches/index.html",
         "@worldCup @IRE see world cup draw results here @WC2018 #wc2018 #russia http://www.fifa.com/worldcup/preliminaries/matches/index.html",
@@ -29,43 +31,45 @@ spams = ["@worldCup @IRE world cup draw results here @WC2018 #wc2018 #russia htt
         "@worldCup @IRE world cup draw results here @WC2018 #wc2018 #russia http://www.fifa.com/worldcup/preliminaries/matches/index.html",
         "@worldCup @IRE follow and retweet to be with a chance of free tickets @WC2018 #wc2018 #russia http://www.fifa.com/worldcup/preliminaries/matches/index.html"]
 
+# calculting the normal editdistances
 normal_vals = []
 for normal1 in normals:
     for normal2 in normals:
         if normal1 != normal2:
             ans = editdistance.eval(normal1, normal2)
             normal_vals.append(ans)
+
+# calculting the spam editdistances
 spam_vals = []
 for spam1 in spams:
     for spam2 in spams:
         if spam1 != spam2:
             ans = editdistance.eval(spam1, spam2)
             spam_vals.append(ans)
+
+# calculting the mixed editdistances
 mixed_vals = []
 for normal in normals:
     for spam in spams:
         ans = editdistance.eval(normal, spam)
         mixed_vals.append(ans)
 
-import matplotlib.pyplot as plt
 count = 1
+# Plot all the values onto the graph
+# Spam plots, red circles
 for spam in spam_vals:
     plt.plot([count], [spam], 'ro')
-    count+=1
+    count += 1
 
-
+# normal plots, blue x's
 for normal in normal_vals:
     plt.plot([count], [normal], 'bx')
-    count+=1
-
+    count += 1
+# Mixture plots, green circles
 for normal in mixed_vals:
     plt.plot([count], [normal], 'go')
-    count+=1
+    count += 1
 
+# label the axis
+plt.ylabel("Levenstein value")
 plt.show()
-
-ans = editdistance.eval("This is spam", "This spam is")
-
-print(ans)
-
-
